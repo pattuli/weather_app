8e1a85f0f9528d0a2a6a1b0836fb0e4450c4466c
Pratik Bastakoti <pratikbastakoti59@gmail.com>
2024-02-21 15:16:57 +0545

    HomeScreen element ultered

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -28,7 +30,7 @@ class HomeScreen extends StatelessWidget {
                     child: Container(
                       height: 300,
                       width: 300,
-                      decoration: BoxDecoration(
+                      decoration: const BoxDecoration(
                           color: Colors.deepPurple, shape: BoxShape.circle),
                     ),
                   ),
@@ -42,12 +44,63 @@ class HomeScreen extends StatelessWidget {
                     ),
                   ),
                   Align(
-                    alignment: AlignmentDirectional(0, -1.2),
+                    alignment: const AlignmentDirectional(0, -1.2),
                     child: Container(
                       height: 300,
                       width: 600,
-                      decoration: BoxDecoration(
+                      decoration: const BoxDecoration(
                           color: Color.fromARGB(255, 241, 198, 25)),
+                    ),
+                  ),
+                  BackdropFilter(
+                      filter: ImageFilter.blur(sigmaX: 100, sigmaY: 100),
+                      child: Container(color: Colors.transparent)),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.height,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text('Pokhara',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.w300)),
+                        const SizedBox(height: 8),
+                        const Text('Good Morning',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 30,
+                                fontWeight: FontWeight.bold)),
+                        Image.asset('assets/1.png'),
+                        const Center(
+                            child: Text(
+                          '20°C',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 55,
+                              fontWeight: FontWeight.w600),
+                        )),
+                        const Center(
+                            child: Text(
+                          'ThunderStrom',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 25,
+                              fontWeight: FontWeight.w500),
+                        )),
+                        const SizedBox(
+                          height: 8,
+                        ),
+                        const Center(
+                          child: Text(
+                            'Wednesday 21 - 9:18 am',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 15,
+                                fontWeight: FontWeight.w300),
+                          ),
+                        )
+                      ],
                     ),
                   )
                 ],
